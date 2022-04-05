68799c3d7bcad188672593862d8762b017d02f2c
Jaine Souza <97619330+ZP045@users.noreply.github.com>
2022-04-05 12:11:23 -0300

    Create App.js

---
+++ Desafios do Bootcamp Congnizant Cloud Data Engineer #2/Frontend/src/App.js
@@ -0,0 +1,12 @@
+import DocumentariesList from './components/DocumentariesList';
+
+const App = () => {
+  return (
+    <>
+      <h1>Seu controle dos Documentários</h1>
+      <DocumentariesList />
+    </>
+  )
+}
+
+export default App;
