9c3f429211370decf86f20f80226d96f8aa8687b
Jaine Souza <97619330+ZP045@users.noreply.github.com>
2022-04-07 12:56:54 -0300

    Create setup.py

---
+++ Desafios do Bootcamp Congnizant Cloud Data Engineer #2/Pacotes de processamento de imagens em Python/setup.py
@@ -0,0 +1,15 @@
+form setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_name",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
