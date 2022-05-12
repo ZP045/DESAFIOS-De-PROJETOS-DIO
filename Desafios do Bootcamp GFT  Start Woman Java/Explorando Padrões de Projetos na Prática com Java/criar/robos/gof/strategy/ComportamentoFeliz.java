1a15f8ec9b907ae8dfc9be9c3c57a23f6d397c85
Jaine Souza <97619330+ZP045@users.noreply.github.com>
2022-05-12 10:34:15 -0300

    Create ComportamentoFeliz.java

---
+++ Desafios do Bootcamp GFT  Start Woman Java/Explorando Padrões de Projetos na Prática com Java/criar/robos/gof/strategy/ComportamentoFeliz.java
@@ -0,0 +1,10 @@
+package criar.robos.gof.strategy;
+
+public class ComportamentoFeliz implements Comportamento {
+
+	@Override
+	public void mover() {
+		System.out.println("Movendo-se bem feliz...");
+	}
+
+}
