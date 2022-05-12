3b7e2ce6b56dd8e86a17abbd41f77baa324d4e58
Jaine Souza <97619330+ZP045@users.noreply.github.com>
2022-05-12 10:37:21 -0300

    Create ComportamentoApaixonado.java

---
+++ Desafios do Bootcamp GFT  Start Woman Java/Explorando Padrões de Projetos na Prática com Java/criar/robos/gof/strategy/ComportamentoApaixonado.java
@@ -0,0 +1,10 @@
+package criar.robos.gof.strategy;
+
+public class ComportamentoApaixonado implements Comportamento {
+
+	@Override
+	public void mover() {
+		System.out.println("Movendo-se apaixonadamente...");
+	}
+
+}
